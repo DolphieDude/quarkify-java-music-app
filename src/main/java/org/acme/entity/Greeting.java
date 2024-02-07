e167a2787edeb2a7c2abf84f12907537c46ff655
DolphieDude <andreycherevat@gmail.com>
2024-02-07 21:42:06 +0100

    using my own db

--- src/main/java/org/acme/entity/Greeting.java
+++ src/main/java/org/acme/entity/Greeting.java
@@ -1,10 +1,17 @@
 package org.acme.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-public class Greeting extends PanacheEntity {
+public class Greeting extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
 
     private String name;
 
